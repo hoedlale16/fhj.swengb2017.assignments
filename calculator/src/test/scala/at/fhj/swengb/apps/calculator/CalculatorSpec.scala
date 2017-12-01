9677e36fe94b946316f37608818f012951238526
Alexander Hoedl <alexander.hoedl@edu.fh-joanneum.at>
2017-12-01 07:45:09 +0100

    config: ongoing development for calculator

--- calculator/src/test/scala/at/fhj/swengb/apps/calculator/CalculatorSpec.scala
+++ calculator/src/test/scala/at/fhj/swengb/apps/calculator/CalculatorSpec.scala
@@ -1,8 +1,52 @@
 package at.fhj.swengb.apps.calculator
 
-class CalculatorSpec {
+import org.scalatest.{Matchers, WordSpecLike}
+
+
+class CalculatorSpec extends WordSpecLike with Matchers {
 
 
   // pending specs ...
+  "validPositiveInt" in {
+    assert(Number("123").getNumber() == 123)
+  }
+
+  "validPositiveInt1" in {
+    assert(Number("1").getNumber() == 1)
+  }
+
+  "validNegativInt" in {
+    assert(Number("-123").getNumber() == -123)
+  }
+
+  "validNegativInt1" in {
+    assert(Number("-1").getNumber() == -1)
+  }
+
+  "invalidNumber0" in {
+    intercept[IllegalArgumentException] {
+      Number("a123")
+    }
+  }
+
+  "invalidNumber1" in {
+    intercept[IllegalArgumentException] {
+      Number("12a3")
+    }
+  }
+
+  "invalidNumber2" in {
+    intercept[IllegalArgumentException] {
+      Number(".123")
+    }
+  }
+
+  "validPositiveDouble" in {
+    assert(Number("123.456").getNumber() == 123.456)
+  }
+
+  "validNegativDouble" in {
+    assert( Number("-123.456").getNumber() == -123.456)
+  }
 
 }
