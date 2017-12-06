336c777b5b3310fa0e684aecdbe1444be31e1cd0
Alexander Hoedl <alexander.hoedl@edu.fh-joanneum.at>
2017-12-06 14:37:59 +0100

    according this awesome documentation continued to do some magic for implementation

--- calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
+++ calculator/src/main/scala/at/fhj/swengb/apps/calculator/RpnCalculator.scala
@@ -19,7 +19,13 @@ object RpnCalculator {
       Try(RpnCalculator())
     else {
       val myStack: List[Op] = s.split(' ').map(e => Op(e)).toList
-      Try(RpnCalculator(myStack))
+      var myCalc: Try[RpnCalculator] = Try(RpnCalculator())
+
+      for (elem <- myStack) {
+        myCalc = myCalc.get.push(elem)
+      }
+
+      myCalc
     }
   }
 
@@ -34,24 +40,34 @@ case class RpnCalculator(stack: List[Op] = Nil) {
 
   /**
     * By pushing Op on the stack, the Op is potentially executed. If it is a Val,  the op instance
-    * is just put on the
-    * stack, if not then the stack is examined and the correct operation is performed.
+    * is just put on the stack,
+    * if not then the stack is examined and the correct operation is performed.
     *
     * @param op
     * @return
     */
   def push(op: Op): Try[RpnCalculator] = {
-    op match {
-      case Val(x) => Try(RpnCalculator((stack :+ op)))
-      case _ => {
-        val value1: Val = stack(0).asInstanceOf[Val];
-        val value2: Val = stack(1).asInstanceOf[Val];
 
-        val result: List[Op] = List(op.asInstanceOf[BinOp].eval(value1,value2))
-        Try(RpnCalculator(result))
-      }
+    if (op.isInstanceOf[Val]) {
+      //Return a new instance of RpnCalculator with a higher stack
+      Try(RpnCalculator(op :: stack))
+    } else {
+
+      //Check if there are still values on stack
+      //TODO: We have to throw here a exception if it is empty
+
+      //Operation detected try to execute it
+      val fstVal = this.pop()._1.asInstanceOf[Val]
+      val sndVal = this.pop()._2.pop()._1.asInstanceOf[Val]
+
+      //new Calc-Obj 2 elements above removed
+      var myStack = this.pop()._2.pop()._2.stack
+
+      val result: Val = op.asInstanceOf[BinOp].eval(sndVal, fstVal)
+      push(result :: myStack)
     }
   }
+
   /**
     * Pushes val's on the stack.
     *
@@ -62,23 +78,19 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     */
   def push(op: Seq[Op]): Try[RpnCalculator] = {
 
-    var myCalc = RpnCalculator(op.toList)
-    for(elem <- myCalc.stack) {
-      if (elem.isInstanceOf[BinOp]) {
-        myCalc = myCalc.push(elem).get
-      }
+    var myCalc = RpnCalculator()
+    for (elem <- op.toList) {
+      myCalc = myCalc.push(elem).get
     }
-
     Try(myCalc)
-
   }
 
   /**
-    * Returns an tuple of Op and a RevPolCal instance with the remainder of the stack.
+    * Returns an tuple of Op and a RpnCalculator instance with the remainder of the stack.
     *
     * @return
     */
-  def pop(): (Op, RpnCalculator) = (stack.head,RpnCalculator(stack.tail))
+  def pop(): (Op, RpnCalculator) = (stack.head, RpnCalculator(stack.tail))
 
   /**
     * If stack is nonempty, returns the top of the stack.
@@ -87,41 +99,10 @@ case class RpnCalculator(stack: List[Op] = Nil) {
     * @return
     */
   def peek(): Op = {
-
-    var myInteralStack: List[Val] = List();
-
-    for( elem <- stack) {
-      //If current element is a Value, add to stack
-      if (elem.isInstanceOf[Val])
-        myInteralStack = elem.asInstanceOf[Val] :: myInteralStack
-      else
-        //If current element is an operation, check if enough values on stack
-        if (myInteralStack.size >= 2 ) {
-          /* If so, take first 2 elements and apply function.
-          *  Add result of function to stack agein...*/
-
-          //Take first elemet from internal stack
-          val fstValue = myInteralStack.head
-          myInteralStack = myInteralStack.tail
-
-          //Take second element from stack
-          val sndValue = myInteralStack.head
-          myInteralStack = myInteralStack.tail
-
-          //Calc result and add result on stack
-          val result = elem.asInstanceOf[BinOp].eval(fstValue,sndValue)
-          myInteralStack = result :: myInteralStack
-
-        } else
-          //Not enoung elements to execute operation!
-          throw new NoSuchElementException()
-    }
-
-    //We're done, return result which is the first element of the internal stack
-    if(myInteralStack.isEmpty)
-      throw new NoSuchElementException()
+    if (stack.isEmpty)
+      throw new NoSuchElementException
     else
-      myInteralStack.head
+      stack.head;
   }
 
   /**
