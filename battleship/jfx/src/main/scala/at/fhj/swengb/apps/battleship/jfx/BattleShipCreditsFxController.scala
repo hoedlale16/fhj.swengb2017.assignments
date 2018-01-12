deddf1dfc55de4622ec51d659fde62d8494a3703
Gregor Fernbach <gregor.fernbach@edu.fh-joanneum.at>
2018-01-12 20:44:21 +0100

    Update to the credit screen.

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipCreditsFxController.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipCreditsFxController.scala
@@ -10,16 +10,34 @@ class BattleShipCreditsFxController extends Initializable {
 
   @FXML private var webView: WebView = _
 
-  private val text: String = "This application was procuded during university course SWENGB of INOFRMATIONMANAGEMENT [IMA16] at FH JOANNEUM.<br>"
+  private val bgcolor: String = "<body bgcolor=\"#61380B\">"
 
-  private val creators: String ="The application was produced by following students and supported by lecturer Dipl.-Ing. Robert Ladstätter." +
-                                "<ul>" +
-                                  "<li>FERNBARCH Gregor</li>" +
-                                  "<li>HOEDL Alexander</li>" +
-                                  "<li>QERIMI Ardian</li>" +
-                                  "<li>WEILAND Sebastian</li>" +
-                                "</ul>"
-  private val sources: String = "All pictures come from following websites:<br>" +
+  private val text: String =  "<h1>Project Description</h1>" +
+                              "This Battleship Game is part of a students' project.\n" +
+                              "It was created for the subject \"Software Engineering Basics (SWENGB)\"\n." +
+                              "This subject is part of the bachelor program Information Management held by the UAS JOANNEUM in the third semester."
+
+  private val bg: String = "<img src=\"/at/fhj/swengb/apps/battleship/jfx/pics/IMA-logo.png\">"
+
+  private val creators: String = "<h1>Project Team</h1>" +
+                                  "<br>The project Team which is responsible for the creation of this application consists of the following persons:" +
+                                  "<ul>" +
+                                    "<li>FERNBACH Gregor</li>" +
+                                    "<li>HOEDL Alexander</li>" +
+                                    "<li>QERIMI Ardian</li>" +
+                                    "<li>WEILAND Sebastian</li>" +
+                                  "</ul>" +
+                                  "Moreover this project was guided and supported by lecturer Dipl.-Ing. Robert Ladstätter. <br>"
+  private val roles: String = "<h1>Project Roles</h1>" +
+                              "The roles of the project team were as follows:" +
+                              "<ul>" +
+                                  "<li>Head of documentation & project management: Fernbach Gregor</li>" +
+                                  "<li>Head of development: Hoedl Alexander</li>" +
+                                  "<li>Head of Design and Layout: Qerimi Ardian</li>" +
+                                  "<li>Head of Testing and quality management: Weiland Sebastian</li>" +
+                              "</ul>"
+  private val sources: String = "<h1>Resources</h1>" +
+                                "Pictures included in this application were extracted from the following websites:<br>" +
                                 "<ul>" +
                                   "<li>https://pixabay.com/p-1283475/?no_redirect</li>" +
                                   "<li>http://wallpaperpulse.com/wallpaper/4028691</li>" +
@@ -27,7 +45,7 @@ class BattleShipCreditsFxController extends Initializable {
                                   "<li>https://www.iconfinder.com/icons/42343/mute_sound_icon#size=32</li>" +
                                   "<li>https://www.iconfinder.com/icons/42341/audio_editing_sound_speaker_volume_icon#size=32</li>" +
                                 "</ul>" +
-                                "All music comes from following websites: <br>"+
+                                "Music & Sound effects included in this application were extracted from the following websites: <br>"+
                                 "<ul>" +
                                   "<li>http://freemusicarchive.org/music/Dee_Yan-Key/Killing_Mozart/06--Dee_Yan-Key-Italian_Nightmare</li>" +
                                   "<li> http://soundbible.com/1986-Bomb-Exploding.html</li>" +
@@ -37,7 +55,7 @@ class BattleShipCreditsFxController extends Initializable {
 
   override def initialize(location: URL, resources: ResourceBundle) = {
     val builder: StringBuilder = new StringBuilder
-    builder.append(text).append(creators).append(sources)
+    builder.append(bgcolor).append(text).append(bg).append(creators).append(roles).append(sources)
 
     webView.getEngine.loadContent(builder.toString())
 
