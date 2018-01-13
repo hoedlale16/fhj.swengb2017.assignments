5f6c94a3f37d8a9a4dba182202d915942c0cb268
Alexander Hoedl <alexander.hoedl@edu.fh-joanneum.at>
2018-01-13 10:14:34 +0100

    refactor: MVC Credit-Screen:  model from view

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipCreditsFxController.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipCreditsFxController.scala
@@ -6,59 +6,17 @@ import javafx.fxml.{FXML, Initializable}
 import javafx.scene.Scene
 import javafx.scene.web.WebView
 
+import at.fhj.swengb.apps.battleship.model.BattleShipGameCredits
+
 class BattleShipCreditsFxController extends Initializable {
 
   @FXML private var webView: WebView = _
 
-  private val bgcolor: String = "<body bgcolor=\"#61380B\">"
-
-  private val text: String =  "<h1>Project Description</h1>" +
-                              "This Battleship Game is part of a students' project.\n" +
-                              "It was created for the subject \"Software Engineering Basics (SWENGB)\"\n." +
-                              "This subject is part of the bachelor program Information Management held by the UAS JOANNEUM in the third semester."
-
-  private val bg: String = "<img src=\"/at/fhj/swengb/apps/battleship/jfx/pics/IMA-logo.png\">"
-
-  private val creators: String = "<h1>Project Team</h1>" +
-                                  "<br>The project Team which is responsible for the creation of this application consists of the following persons:" +
-                                  "<ul>" +
-                                    "<li>FERNBACH Gregor</li>" +
-                                    "<li>HOEDL Alexander</li>" +
-                                    "<li>QERIMI Ardian</li>" +
-                                    "<li>WEILAND Sebastian</li>" +
-                                  "</ul>" +
-                                  "Moreover this project was guided and supported by lecturer Dipl.-Ing. Robert Ladstätter. <br>"
-  private val roles: String = "<h1>Project Roles</h1>" +
-                              "The roles of the project team were as follows:" +
-                              "<ul>" +
-                                  "<li>Head of documentation & project management: Fernbach Gregor</li>" +
-                                  "<li>Head of development: Hoedl Alexander</li>" +
-                                  "<li>Head of Design and Layout: Qerimi Ardian</li>" +
-                                  "<li>Head of Testing and quality management: Weiland Sebastian</li>" +
-                              "</ul>"
-  private val sources: String = "<h1>Resources</h1>" +
-                                "Pictures included in this application were extracted from the following websites:<br>" +
-                                "<ul>" +
-                                  "<li>https://pixabay.com/p-1283475/?no_redirect</li>" +
-                                  "<li>http://wallpaperpulse.com/wallpaper/4028691</li>" +
-                                  "<li>https://www.flickr.com/photos/verfain/6388934159/</li>" +
-                                  "<li>https://www.iconfinder.com/icons/42343/mute_sound_icon#size=32</li>" +
-                                  "<li>https://www.iconfinder.com/icons/42341/audio_editing_sound_speaker_volume_icon#size=32</li>" +
-                                "</ul>" +
-                                "Music & Sound effects included in this application were extracted from the following websites: <br>"+
-                                "<ul>" +
-                                  "<li>http://freemusicarchive.org/music/Dee_Yan-Key/Killing_Mozart/06--Dee_Yan-Key-Italian_Nightmare</li>" +
-                                  "<li> http://soundbible.com/1986-Bomb-Exploding.html</li>" +
-                                  "<li>http://soundbible.com/1463-Water-Balloon.html</li>" +
-                                "</ul>"
-
 
   override def initialize(location: URL, resources: ResourceBundle) = {
-    val builder: StringBuilder = new StringBuilder
-    builder.append(bgcolor).append(text).append(bg).append(creators).append(roles).append(sources)
-
-    webView.getEngine.loadContent(builder.toString())
-
+    //Load credits and show it
+    val credits: String = BattleShipGameCredits().getCreditText
+    webView.getEngine.loadContent(credits)
   }
 
   /**
---
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/model/BattleShipGameCredits.scala
@@ -0,0 +1,76 @@
+package at.fhj.swengb.apps.battleship.model
+
+import java.io.File
+
+case class BattleShipGameCredits() {
+
+  private def appendHeaderText(builder: StringBuilder): StringBuilder =  {
+    builder.append("<h1>Project Description</h1>")
+           .append("This Battleship Game is part of a students' project.\n")
+           .append("It was created for the subject \"Software Engineering Basics (SWENGB)\"\n." )
+           .append("This subject is part of the bachelor program Information Management held by the UAS JOANNEUM in the third semester.")
+
+    val imaLogo: File = new File(getClass.getResource("/at/fhj/swengb/apps/battleship/jfx/pics/FH_IMA_Logo.jpg").getFile)
+    builder.append("<br><center><img src=\""+imaLogo.toURI+"\" width='300' /></center><br>")
+
+  }
+
+  private def appendTeamMembers(builder: StringBuilder): StringBuilder = {
+    builder.append("<h1>Project Team</h1>")
+      .append("<br>The project Team which is responsible for the creation of this application consists of the following persons:")
+      .append("<ul>")
+      .append("<li>FERNBACH Gregor</li>")
+      .append("<li>HOEDL Alexander</li>")
+      .append("<li>QERIMI Ardian</li>")
+      .append("<li>WEILAND Sebastian</li>")
+      .append("</ul>")
+      .append("Moreover this project was guided and supported by lecturer Dipl.-Ing. Robert Ladstätter. <br>")
+  }
+
+  private def appendTeamRoles(builder: StringBuilder): StringBuilder = {
+    builder.append("<h1>Project Roles</h1>")
+      .append("The roles of the project team were as follows:")
+      .append("<ul>")
+      .append("<li>Head of documentation & project management: Fernbach Gregor</li>")
+      .append("<li>Head of development: Hoedl Alexander</li>")
+      .append("<li>Head of Design and Layout: Qerimi Ardian</li>")
+      .append("<li>Head of Testing and quality management: Weiland Sebastian</li>")
+      .append("</ul>")
+  }
+
+
+  private def appendExternalSources(builder: StringBuilder): StringBuilder = {
+    builder.append("<h1>Resources</h1>")
+      .append("Pictures included in this application were extracted from the following websites:<br>")
+      .append("<ul>")
+      .append("<li>href=https://pixabay.com/p-1283475/?no_redirect</li>")
+      .append("<li>http://wallpaperpulse.com/wallpaper/4028691</li>")
+      .append("<li>https://www.flickr.com/photos/verfain/6388934159/</li>")
+      .append("<li>https://www.iconfinder.com/icons/42343/mute_sound_icon#size=32</li>")
+      .append("<li>https://www.iconfinder.com/icons/42341/audio_editing_sound_speaker_volume_icon#size=32</li>")
+      .append("</ul>")
+      .append("Music & Sound effects included in this application were extracted from the following websites: <br>")
+      .append("<ul>")
+      .append("<li>http://freemusicarchive.org/music/Dee_Yan-Key/Killing_Mozart/06--Dee_Yan-Key-Italian_Nightmare</li>")
+      .append("<li> http://soundbible.com/1986-Bomb-Exploding.html</li>")
+      .append("<li>http://soundbible.com/1463-Water-Balloon.html</li>")
+      .append("</ul>")
+  }
+
+  /**
+    * Returns Credits as a string
+    * @return
+    */
+  def getCreditText: String = {
+    val credits: StringBuilder = new StringBuilder("<body bgcolor=\"#61380B\">")
+
+    //Append text modules
+    appendHeaderText(credits)
+    appendTeamMembers(credits)
+    appendTeamRoles(credits)
+    appendExternalSources(credits)
+
+    credits.toString()
+  }
+
+}
